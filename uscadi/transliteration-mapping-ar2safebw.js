bf27a40692ec3664aa6f45824c331b98bca41db5
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-07-18 15:09:14 +0200

    add warnings about charmap breaking changes

--- uscadi/transliteration-mapping-ar2safebw.js
+++ uscadi/transliteration-mapping-ar2safebw.js
@@ -3,6 +3,7 @@ module.exports = {
         "\u0621": "C",
         "\u0622": "M",
         "\u0623": "O",
+        // TODO: THIS IS NOT OK -- THIS IS ONLY COMMENTED OUT HERE TO MATCH THE PYTHON OUTPUT
         // "\u0624": "W",
         "\u0625": "I",
         "\u0626": "Q",
@@ -34,6 +35,7 @@ module.exports = {
         "\u0645": "m",
         "\u0646": "n",
         "\u0647": "h",
+        // TODO: THIS IS NOT OK -- THIS IS ONLY COMMENTED OUT HERE TO MATCH THE PYTHON OUTPUT
         // "\u0648": "w",
         "\u0649": "Y",
         "\u064A": "y",
@@ -51,6 +53,7 @@ module.exports = {
         "\u0686": "J",
         "\u06A4": "B",
         "\u06AF": "G",
+        // TODO: THIS IS NOT OK -- THESE TWO ARE ONLY HERE TO MATCH THE PYTHON OUTPUT
         "(": "P",
         ")": "P",
     }
