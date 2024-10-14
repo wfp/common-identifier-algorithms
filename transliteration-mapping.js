6f6dcbcf48a8a94d96089a80693f1bb7a098148d
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2024-10-14 12:09:45 +0200

    added GNU GPLv3 license

--- transliteration-mapping.js
+++ transliteration-mapping.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Building Blocks CommonID Tool
+ * Copyright (c) 2024 WFP
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 // Transliteration map (one to one mode) between each normalized Arabic letter
 // (no hamza and tah marbouta) represented in the id property and English letter
 // (capital letter) in the content of the item tag
